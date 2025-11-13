9dde9b499c5c5b718a0aaec207338f97759bda7f
Choijichan <sjy08007@gmail.com>
2025-11-13 13:04:55 +0900

    Create di.dart

---
+++ core/di.dart
@@ -0,0 +1,18 @@
+import 'package:get_it/get_it.dart';
+import '../data/drift/drift_database.dart';
+import '../data/repositories/diary_repository.dart';
+import '../data/repositories/schedule_repository.dart';
+
+/// GetIt DI 컨테이너
+final GetIt getIt = GetIt.instance;
+
+/// 앱 시작 시 한 번만 호출
+Future<void> setupDI() async {
+  // 1) DB 인스턴스
+  final db = LocalDatabase();
+  getIt.registerSingleton<LocalDatabase>(db);
+
+  // 2) Repository
+  getIt.registerLazySingleton<DiaryRepository>(() => DiaryRepository(getIt()));
+  getIt.registerLazySingleton<ScheduleRepository>(() => ScheduleRepository(getIt()));
+}
