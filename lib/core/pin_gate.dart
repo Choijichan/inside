ed93981d2d57246700a0cb6dbea7598a0c4d9440
choijichan <sjy08007@gmail.com>
2025-11-25 13:49:56 +0900

    Updated Changes

--- lib/core/pin_gate.dart
+++ lib/core/pin_gate.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'pin_lock.dart';
-import '../features/lock/lock_screen.dart';
-import '../main.dart'; // RootScaffold
+import '../root_scaffold.dart'; // ✅ 여기로 수정
 
 class PinGate extends StatelessWidget {
   const PinGate({super.key});
@@ -18,7 +17,7 @@ class PinGate extends StatelessWidget {
         }
 
         if (pinLock.hasPin) {
-          return const LockScreen();
+          return const RootScaffold(); // 이제 에러 안 남
         }
 
         return const RootScaffold();
